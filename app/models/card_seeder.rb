9e1467dacea33f9be65faab3e87504bb3e57b98c
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-05-01 10:54:41 +0000

    Pontoon: Update Croatian (hr) localization of AMO

--- app/models/card_seeder.rb
+++ app/models/card_seeder.rb
@@ -1,2 +1,9 @@
 class CardSeeder
-  d
+  def self.seed_card_set_from_json(file_name)
+    opened_file = File.read(file_name)
+    card_set_in_json = JSON.parse(opened_file)
+
+    new_card_set = CardSet.create(
+      name: card_set_in_json['name'],
+      code: card_set_in_json['code'],
+      release_date: card_set_in_json['releas
