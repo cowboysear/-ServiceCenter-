1d00532f9d0241eb77f6d88953459f977153ad8f
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-05-07 15:59:36 +0000

    Remove debug logging

--- app/models/card_set.rb
+++ app/models/card_set.rb
@@ -3,4 +3,16 @@
 # Table name: card_sets
 #
 #  id           :integer          not null, primary key
-#  name  
+#  name         :string(255)
+#  code         :string(255)
+#  release_date :date
+#  border       :string(255)
+#  type         :string(255)
+#  created_at   :datetime
+#  updated_at   :datetime
+#
+
+class CardSet < ActiveRecord::Base
+  validates :name, presence: true
+  validates :code, presence: true
+  validate
