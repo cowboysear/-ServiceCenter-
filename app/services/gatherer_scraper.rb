a60574efa841ecccff053d90d399ef9910fd36be
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-05-09 18:10:52 +0000

    typo in link

--- app/services/gatherer_scraper.rb
+++ app/services/gatherer_scraper.rb
@@ -12,4 +12,10 @@ class GathererScraper
   def scrape!(uri)
     parsed_html = Nokogiri::HTML(open("http://gatherer.wizards.com#{uri}"))
 
-    # Cards are listed in .cardItemTable, .cardItem represents an 
+    # Cards are listed in .cardItemTable, .cardItem represents an individual card. We iterate over it to get the card_image_uri
+    card_items = parsed_html.css('.cardItem')
+    card_items.each do |ci|
+      image_uri = ci.search('img').first.attributes['src'].value
+      card_name = ci.css('.cardTitle').children.last.children.text
+
+      Card.where(name: card_name).update_all(card_i
