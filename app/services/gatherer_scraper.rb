75471ea808379c3ab35eef08dc7bfdc94918c84d
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-05-09 11:55:35 +0000

    sdasd

--- app/services/gatherer_scraper.rb
+++ app/services/gatherer_scraper.rb
@@ -2,4 +2,14 @@ require 'open-uri'
 require 'nokogiri'
 
 # Scrapes Gatherer's API for images
-class Gath
+class GathererScraper
+  GATHERER_URL = 'http://gatherer.wizards.com/Pages/Search/Default.aspx?'
+
+  def self.scrape!(uri)
+    new.scrape!(uri)
+  end
+
+  def scrape!(uri)
+    parsed_html = Nokogiri::HTML(open("http://gatherer.wizards.com#{uri}"))
+
+    # Cards are listed in .cardItemTable, .cardItem represents an 
