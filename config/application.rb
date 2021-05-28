36c88a19e94024b62db2b67e04c4b04e42262c57
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-05-28 12:51:40 +0000

    front-end theme

--- config/application.rb
+++ config/application.rb
@@ -1,1 +1,13 @@
-require File
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module JadeMtg
+  class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_fra
