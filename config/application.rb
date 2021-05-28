2417601c030a0cf2a6f393a72b33a3927ae6729c
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-05-28 13:15:22 +0000

    POC

--- config/application.rb
+++ config/application.rb
@@ -10,4 +10,14 @@ module JadeMtg
   class Application < Rails::Application
 
     config.generators do |g|
-      g.test_fra
+      g.test_framework :rspec,
+        fixtures: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false,
+        request_specs: false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
+
+    # Settings in config/environment
