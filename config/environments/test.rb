5b34322f38c6a8c9b6cd0c62cc786cdb887ee7b1
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-06-07 19:23:19 +0000

    Remove more dead code.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -16,4 +16,12 @@ Rails.application.configure do
   config.serve_static_files   = true
   config.static_cache_control = 'public, max-age=3600'
 
-  # Show full e
+  # Show full error reports and disable caching.
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Raise exceptions instead of rendering exception templates.
+  config.action_dispatch.show_exceptions = false
+
+  # Disable request forgery protection in test environment.
+  config.action_controller
