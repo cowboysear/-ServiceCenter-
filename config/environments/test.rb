6c30312e44f798f3f1d9e6aed957df0343ddd9ad
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-06-07 08:53:11 +0000

    Commit inicial.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -3,4 +3,10 @@ Rails.application.configure do
 
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
-  # your test database is "scratch
+  # your test database is "scratch space" for the test suite and is wiped
+  # and recreated between test runs. Don't rely on the data there!
+  config.cache_classes = true
+
+  # Do not eager load code on boot. This avoids loading your whole application
+  # just for the purpose of running a single test. If you are using a tool that
+  # pre
