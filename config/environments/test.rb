4c5c2af7ff4a9990da8d7c4eb0566eda9c51abf1
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-06-07 09:34:04 +0000

    Removed unused images.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -9,4 +9,11 @@ Rails.application.configure do
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
-  # pre
+  # preloads Rails for running tests, you may have to set it to true.
+  config.eager_load = false
+
+  # Configure static file server for tests with Cache-Control for performance.
+  config.serve_static_files   = true
+  config.static_cache_control = 'public, max-age=3600'
+
+  # Show full e
