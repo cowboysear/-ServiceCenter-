e9411cb811c7eacee1a8e93bb1886eb6e56c9725
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-06-10 18:36:16 +0000

    conflicts resolved

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -5,4 +5,12 @@
 # locales as you wish. All of these examples are active by default:
 # ActiveSupport::Inflector.inflections(:en) do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
-#   inflect
+#   inflect.singular /^(ox)en/i, '\1'
+#   inflect.irregular 'person', 'people'
+#   inflect.uncountable %w( fish sheep )
+# end
+
+# These inflection rules are supported but not enabled by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.acronym 'RESTful'
+# end
