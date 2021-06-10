3be6662b1e3a6141e0766d79075ef74a1dff2076
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-06-10 08:59:36 +0000

    2007-11-21  David Gilbert  <david.gilbert@object-refinery.com>

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,1 +1,8 @@
-# Be sure to restart your server when you m
+# Be sure to restart your server when you modify this file.
+
+# Add new inflection rules using the following format. Inflections
+# are locale specific, and you may define rules for as many different
+# locales as you wish. All of these examples are active by default:
+# ActiveSupport::Inflector.inflections(:en) do |inflect|
+#   inflect.plural /^(ox)$/i, '\1en'
+#   inflect
