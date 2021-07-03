7a75a5494035dfcf345e2fb4cba2067ae41927c2
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-07-03 22:39:42 +0000

    redesign

--- db/migrate/20151112223942_devise_create_users.rb
+++ db/migrate/20151112223942_devise_create_users.rb
@@ -12,4 +12,14 @@ class DeviseCreateUsers < ActiveRecord::Migration
       ## Rememberable
       t.datetime :remember_created_at
 
-      ## Tr
+      ## Trackable
+      t.integer  :sign_in_count, default: 0, null: false
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
+      ## Confirmable
+      # t.string   :confirmation_token
+      # t.datetime :confirmed_at
+      # t.datetime :
