243d56392133367893ecf19f52be0f8365e1e679
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-07-03 14:58:30 +0000

    wireless: rt2x00: add new rt2800usb device

--- db/migrate/20151112223942_devise_create_users.rb
+++ db/migrate/20151112223942_devise_create_users.rb
@@ -2,4 +2,14 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :ema
+      t.string :email,              null: false, default: ""
+      t.string :encrypted_password, null: false, default: ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Tr
