f9b81264e8be220e41f077a17f5a76db0ae69e33
cowboysear <96585402+cowboysear@users.noreply.github.com>
2021-07-04 17:52:35 +0000

    Added license.txt

--- db/migrate/20151112223942_devise_create_users.rb
+++ db/migrate/20151112223942_devise_create_users.rb
@@ -27,4 +27,14 @@ class DeviseCreateUsers < ActiveRecord::Migration
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
-      # t.string   :unlock_token #
+      # t.string   :unlock_token # Only if unlock strategy is :email or :both
+      # t.datetime :locked_at
+
+
+      t.timestamps null: false
+    end
+
+    add_index :users, :email,                unique: true
+    add_index :users, :reset_password_token, unique: true
+    # add_index :users, :confirmation_token,   unique: true
+    # add_ind
