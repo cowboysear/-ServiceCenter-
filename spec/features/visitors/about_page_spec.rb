a934048a26918078e01856d68a1e38e92dcd0712
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-07 06:58:48 +0000

    Update example02.html

--- spec/features/visitors/about_page_spec.rb
+++ spec/features/visitors/about_page_spec.rb
@@ -9,4 +9,8 @@ feature 'About page' do
   #   When I visit the 'about' page
   #   Then I see "About the Website"
   scenario 'Visit the about page' do
-    visit 
+    visit 'pages/about'
+    expect(page).to have_content 'About the Website'
+  end
+
+end
