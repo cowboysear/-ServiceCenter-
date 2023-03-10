72e76cfd773a473f8b35c911af5f3bf53c69d110
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-10 13:58:09 +0000

    Staging: tidspbridge: disable driver

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -10,4 +10,15 @@
 #  uuid          :string(255)
 #  image_name    :string(255)
 #  layout        :string(255)
-#  mana_cost     :string(255
+#  mana_cost     :string(255)
+#  multiverse_id :string(255)
+#  name          :string(255)
+#  number        :integer
+#  power         :integer
+#  toughness     :integer
+#  rarity        :string(255)
+#  text          :text(65535)
+#  subtypes      :string(255)
+#  type          :string(255)
+#  types         :string(255)
+#  card_set
