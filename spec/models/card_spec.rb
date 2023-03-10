8f183e75ba524df92e021496680652feec8352a4
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-10 09:52:00 +0000

    ipvs: uninitialized data with IP_VS_IPV6

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,4 +2,12 @@
 #
 # Table name: cards
 #
-#  id           
+#  id            :integer          not null, primary key
+#  artist        :string(255)
+#  cmc           :integer
+#  colors        :string(255)
+#  flavor        :text(65535)
+#  uuid          :string(255)
+#  image_name    :string(255)
+#  layout        :string(255)
+#  mana_cost     :string(255
