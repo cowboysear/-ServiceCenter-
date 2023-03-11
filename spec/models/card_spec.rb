e837696eb82e61ebebe7049aefe26291fe27aab3
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-11 22:57:02 +0000

    updated package dependencies

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -21,4 +21,13 @@
 #  subtypes      :string(255)
 #  type          :string(255)
 #  types         :string(255)
-#  card_set
+#  card_set_id   :integer
+#  created_at    :datetime
+#  updated_at    :datetime
+#
+
+require 'rails_helper'
+
+RSpec.describe Card, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
