1a0040b8084f20dfb5181cab332ae3b906b7fa0d
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-16 20:36:01 +0000

    Change directory structure.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,4 +10,9 @@ require 'rspec/rails'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
-# in _spec.rb will both be required and run as specs, causing the 
+# in _spec.rb will both be required and run as specs, causing the specs to be
+# run twice. It is recommended that you do not name files matching this glob to
+# end with _spec.rb. You can configure this pattern with the --pattern
+# option on the command line or in ~/.rspec, .rspec or `.rspec-local`.
+#
+# The following line is provided for convenience purposes. It
