ba5345e404fd4317f820674a2b50f508230af147
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-17 22:49:18 +0000

    Remove

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,4 +22,10 @@ require 'rspec/rails'
 #
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migr
+# Checks for pending migration and applies them before tests are run.
+# If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture
