9eca84e911b92881dbbe57939bc37cc707c2f12e
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-23 13:07:48 +0000

    fixing wms overlay, maptiles_geoext_test.html

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,4 +7,10 @@
 # Given that it is always loaded, you are encouraged to keep this file as
 # light-weight as possible. Requiring heavyweight dependencies from this file
 # will add to the boot time of your test suite on EVERY test run, even for an
-# individual file that may not 
+# individual file that may not need all of that loaded. Instead, consider making
+# a separate helper file that requires the additional dependencies and performs
+# the additional setup, and require it from the spec files that actually need
+# it.
+#
+# The `.rspec` file also contains a few flags that are not defaults but that
+# users common
