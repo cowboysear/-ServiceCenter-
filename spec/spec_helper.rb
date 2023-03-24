b8dab4234c8a15eb9ab17467ddda81a667fdc95b
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-03-24 07:51:28 +0000

    updated version and changelog

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,4 +20,9 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  config.exp
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 
