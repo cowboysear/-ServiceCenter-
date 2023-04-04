80d86df0f44a47c6b280797531df9bdeb9d6c878
cowboysear <96585402+cowboysear@users.noreply.github.com>
2023-04-04 12:10:02 +0000

    Update working

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -7,4 +7,15 @@ RSpec.configure do |config|
     DatabaseCleaner.strategy = :transaction
   end
 
-  
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+end
